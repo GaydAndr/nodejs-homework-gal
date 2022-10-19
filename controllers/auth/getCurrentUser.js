3aa0e6508a01a95d1864a8b0bab3d82045d6341c
andrii_h <galgayand@gmail.com>
2022-10-20 01:22:58 +0300

    del log

--- controllers/auth/getCurrentUser.js
+++ controllers/auth/getCurrentUser.js
@@ -2,8 +2,6 @@ const { User } = require('../../models/user');
 const { RequestError } = require('../../helpers');
 
 const getCurrentUser = async (req, res) => {
-  console.log(1231);
-
   const user = await User.findById(req.user._id);
   if (!user) {
     throw new RequestError(401, 'Not authorized');
